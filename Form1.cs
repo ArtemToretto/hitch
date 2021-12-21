6e8471df104ac8b3903faab4f3df179d20838fea
Артём Чернов <artem_chernow@mail.ru>
2021-12-21 21:00:55 +0800

    Создал класс частиц, а список частиц, добавил туда 500 частичек..

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,26 @@ namespace hitch
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
+
         public Form1()
         {
             InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (int i=0;i<500;i++)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
+            }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Particle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hitch
+{
+    class Particle
+    {
+        public int radius;
+        public float X;
+        public float Y;
+        public float direction; //направление
+        public float speed;
+
+        public static Random Random = new Random();
+
+        public Particle()
+        {
+            radius = 2 + Random.Next(10);
+            speed = 1 + Random.Next(10);
+            direction = Random.Next(360);
+        }
+    }
+}
